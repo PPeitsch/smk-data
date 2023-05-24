d172744ac607cae51fcb50c0c522e0a9de2725f4
PPeitsch <preynosopeitsch@estudiantes.unsam.edu.ar>
2023-05-24 13:03:36 -0300

    some changes on csv files and dataframe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
 data_folder = "data/"
 
@@ -9,18 +11,26 @@ encabezado = ["Fecha", "Hora"]
 
 def open_csv(file_name):
     with open(file=file_name, mode='rt', encoding="utf8") as f:
-        filas = csv.reader(f, delimiter=";")
+        filas = csv.reader(f, delimiter=",")
         list_temp = [{name: i for name, i in zip(encabezado, fila)} for n_fila, fila in enumerate(filas, start=1)]
 
     return list_temp
 
 
-files_list = [open_csv(data_folder+n) for n in files]
-#print(f'Una lista de diccionarios:\n{files_list}')
+files_list = [open_csv(data_folder + n) for n in files]
 
-longitudes = [len(lista) for n, lista in enumerate(files_list)]
-print(f'longitudes: {longitudes}')
+complete_list = [[row for n_row, row in enumerate(mlista) if not row['Fecha'] == ""]
+                 for n, mlista in enumerate(files_list)]
+result = [a for b in complete_list for a in b]
 
-filas_vacias = [{n_row: row for n_row, row in enumerate(lista)} for n, lista in enumerate(files_list)]
-otras_longitudes = [len(lista_vacios) for n, lista_vacios in enumerate(filas_vacias)]
-print(f'filas vacias: {otras_longitudes}')
+df = pd.DataFrame(result)
+df['Hora'] = df['Hora'].str.replace("h","")
+df['Fecha'] = pd.to_datetime(df['Fecha'], format="%d/%m/%Y")
+df['Hora'] = pd.to_datetime(df['Hora'], format="%H.%M")
+
+print(df.head())
+print(df.dtypes)
+
+plt.figure(1)
+plt.plot(df['Fecha'], df['Hora'])
+plt.show()
